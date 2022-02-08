18c24acaef1d1f5619b6f00d917af83dc837c4bc
Will-Harris00 <wjharris00@outlook.com>
2022-02-08 14:21:48 +0000

    WolframAlpha Short Answer API Microservice

---
+++ alpha.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+const (
+	URI = "http://api.wolframalpha.com/v1/result"
+	KEY = "DEMO"
+)
+
+func Alpha(w http.ResponseWriter, r *http.Request) {
+	t := map[string]interface{}{}
+	if err := json.NewDecoder(r.Body).Decode(&t); err == nil {
+		if text_query, ok := t["text"].(string); ok {
+			if response, err := Service(text_query); err == nil {
+				u := map[string]interface{}{"text": response}
+				w.WriteHeader(http.StatusOK)
+				json.NewEncoder(w).Encode(u)
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
+func Service(text_query string) (interface{}, error) {
+	//client := &http.Client{}
+	uri := URI + "?appid=" + KEY + "&i=" + url.QueryEscape(text_query) // html encoded string
+	println(uri)
+	//if req, err := http.NewRequest("GET", uri, nil); err == nil {
+	//	if rsp, err := client.Do(req); err == nil {
+	//		if rsp.StatusCode == http.StatusOK {
+	//			t := map[string]interface{}{}
+	//			if err := json.NewDecoder(rsp.Body).Decode(&t); err == nil {
+	//				return t["response"], nil
+	//			}
+	//		}
+	//	}
+	//}
+	response, err := http.Get(uri)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close() // delay the execution of the function until the nearby functions returns
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	responseString := string(responseData)
+
+	println(response.Body)
+	println(responseData)
+	println(responseString)
+
+	return responseString, nil
+	//return nil, errors.New("Service")
+}
+
+func main() {
+	r := mux.NewRouter()
+	// document
+	r.HandleFunc("/alpha", Alpha).Methods("POST")
+	http.ListenAndServe(":3001", r)
+}
--- alphatest.sh
+++ alphatest.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-JSON="{\"text\":\"What is the melting point of silver?\"}"
+# JSON="{\"text\":\"What is the melting point of silver?\"}"
+JSON="{\"text\":\"How far is Los Angeles from New York?\"}"
 JSON2=`curl -s -X POST -d "$JSON" localhost:3001/alpha`
 echo $JSON2
