464d0289b38c372fe113271701e81c1bacacb83e
Will-Harris00 <wjharris00@outlook.com>
2022-02-10 18:17:28 +0000

    Export Encoded Answer Speech to Audio File

--- alexa.go
+++ alexa.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"io"
 	"io/ioutil"
@@ -11,7 +12,8 @@ import (
 func Alexa(w http.ResponseWriter, r *http.Request) {
 	stt_rsp := SpeechToTextManager(r)
 	alpha_rsp := AlphaManager(stt_rsp)
-	TextToSpeechManager(alpha_rsp)
+	tts_rsp_body := TextToSpeechManager(alpha_rsp)
+	AlexaResponse(w, tts_rsp_body)
 }
 
 func SpeechToTextManager(r *http.Request) []byte {
@@ -23,14 +25,14 @@ func SpeechToTextManager(r *http.Request) []byte {
 	rsp, _ := http.DefaultClient.Do(req)
 
 	if rsp.StatusCode != http.StatusOK {
-		panic("something went wrong!")
+		panic("Something went wrong with the speech-to-text microservice!")
 	}
 
 	defer rsp.Body.Close()
 
 	if rsp.StatusCode == http.StatusOK {
 		rsp_body, _ := ioutil.ReadAll(rsp.Body)
-		println(string(rsp_body)) // check response is received from speech-to-text microservice
+		// println(string(rsp_body)) // check response is received from speech-to-text microservice
 		return rsp_body
 	}
 	return nil
@@ -45,32 +47,45 @@ func AlphaManager(stt_rsp []byte) io.ReadCloser {
 	alpha_rsp, _ := http.DefaultClient.Do(req)
 
 	if alpha_rsp.StatusCode != http.StatusOK {
-		panic("something went wrong!")
+		panic("Something went wrong with the alpha microservice!")
 	}
 
 	return alpha_rsp.Body
 }
 
-func TextToSpeechManager(tts_rsp io.ReadCloser) []byte {
+func TextToSpeechManager(alpha_rsp io.ReadCloser) []byte {
 	tts_uri := "http://localhost:3003/tts"
 
-	req, _ := http.NewRequest("POST", tts_uri, tts_rsp)
+	req, _ := http.NewRequest("POST", tts_uri, alpha_rsp)
 	req.Header.Set("Content-Type", "application/json")
 
-	rsp, _ := http.DefaultClient.Do(req)
+	tts_rsp, _ := http.DefaultClient.Do(req)
 
-	if rsp.StatusCode != http.StatusOK {
-		panic("something went wrong!")
+	if tts_rsp.StatusCode != http.StatusOK {
+		panic("Something went wrong with the text-to-speech microservice!")
 	}
 
-	defer rsp.Body.Close()
+	defer tts_rsp.Body.Close()
 
-	if rsp.StatusCode == http.StatusOK {
-		rsp_body, _ := ioutil.ReadAll(rsp.Body)
-		println(string(rsp_body)) // check response is received from speech-to-text microservice
+	if tts_rsp.StatusCode == http.StatusOK {
+		rsp_body, _ := ioutil.ReadAll(tts_rsp.Body)
+		// println(string(rsp_body)) // check response is received from speech-to-text microservice
 		return rsp_body
 	}
 	return nil
+}
+
+func AlexaResponse(w http.ResponseWriter, tts_rsp_body []byte) {
+	t := map[string]interface{}{}
+	err := json.Unmarshal(tts_rsp_body, &t)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json") // return microservice response as json
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(t)
+	println("Play the answer.wav file to hear the solution to your question!")
 }
 
 func AlexaHandler() {
