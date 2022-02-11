6999bc8c8be110ebd7a108567763bd20c7e48e2a
Will-Harris00 <wjharris00@outlook.com>
2022-02-11 15:22:35 +0000

    Handled Response Codes from WolframAlpha Short Answers API

--- alpha.go
+++ alpha.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 )
@@ -14,50 +13,64 @@ const (
 	KEY = ""
 )
 
-func Alpha(w http.ResponseWriter, r *http.Request) {
-	t := map[string]interface{}{}
-	if err := json.NewDecoder(r.Body).Decode(&t); err == nil {
-		if text_query, ok := t["text"].(string); ok {
-			if response, err := AlphaService(text_query); err == nil {
-				u := map[string]interface{}{"text": response}
-				w.Header().Set("Content-Type", "application/json") // return microservice response as json
-				w.WriteHeader(http.StatusOK)
-				json.NewEncoder(w).Encode(u) // encode string text as json object
-			} else {
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-		} else {
-			w.WriteHeader(http.StatusBadRequest)
-		}
-	} else {
-		w.WriteHeader(http.StatusBadRequest)
+func check(e error) {
+	if e != nil {
+		panic(e)
 	}
 }
 
-func AlphaService(text_query string) (interface{}, error) {
-	println(text_query)                                                      // check the question
-	alpha_uri := URI + "?appid=" + KEY + "&i=" + url.QueryEscape(text_query) // html encoded string
+func Alpha(w http.ResponseWriter, r *http.Request) {
+	t := map[string]interface{}{}
+	err := json.NewDecoder(r.Body).Decode((&t))
+	check(err)
 
-	response, err := http.Get(alpha_uri)
-	if err != nil {
-		log.Fatal(err)
-	}
+	textQuery := t["text"].(string)
 
-	defer response.Body.Close() // delay the execution of the function until the nearby functions returns
+	alphaResponse, wolframStatus := AlphaService(textQuery)
 
-	responseData, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+	u := map[string]interface{}{"text": alphaResponse}
+	w.Header().Set("Content-Type", "application/json") // return microservice response as json
+	w.WriteHeader(wolframStatus)                       // copy the status code returned from wolfram alpha short answers api
+	json.NewEncoder(w).Encode(u)                       // encode string text as json object
+}
+
+func AlphaService(textQuery string) (interface{}, int) {
+	println(textQuery)                                                     // check the question
+	alphaURI := URI + "?appid=" + KEY + "&i=" + url.QueryEscape(textQuery) // html encoded string
+
+	wolframResponse, err := http.Get(alphaURI)
+	check(err)
+
+	CheckStatusError(wolframResponse.StatusCode)
+	// println(wolframResponse.StatusCode) // determine if the wolfram alpha api returned the correct response
+
+	defer wolframResponse.Body.Close() // delay the execution of the function until the nearby functions returns
+
+	responseData, err := ioutil.ReadAll(wolframResponse.Body) // read the body of the response returned from the wolfram api
+	check(err)
 
 	responseString := string(responseData)
-
-	// println(response.Body)
-	// println(responseData)
 	println(responseString)
 
-	return responseString, nil
-	//return nil, errors.New("Service")
+	return responseString, wolframResponse.StatusCode
+}
+
+func CheckStatusError(status int) {
+	switch status {
+	case http.StatusBadRequest: // 400 - No input.  Please specify the input using the 'i' query parameter.
+		println("This status indicates that the API did not find an input parameter while parsing. " +
+			"In most cases, this can be fixed by checking that you have used the correct syntax for " +
+			"including the i parameter.")
+	case http.StatusForbidden: // 403 - Error 1: Invalid appid or Error 2: Appid missing
+		println("This error is returned when a request contains an invalid option for the appid parameter. " +
+			"Double-check that your AppID is typed correctly and that your appid parameter is using the correct syntax.")
+	case http.StatusNotImplemented: // 501 - No short answer available
+		println("This status is returned if a given input value cannot be interpreted by this API. " +
+			"This is commonly caused by input that is misspelled, poorly formatted or otherwise unintelligible. " +
+			"Because this API is designed to return a single result, this message may appear if no sufficiently " +
+			"short result can be found. You may occasionally receive this status when requesting information on " +
+			"topics that are restricted or not covered.")
+	}
 }
 
 func main() {
