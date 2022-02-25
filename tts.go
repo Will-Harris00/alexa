caccbfedc7d2f34c969d071f5fc6f322fdaafc81
Will-Harris00 <wjharris00@outlook.com>
2022-02-25 15:34:05 +0000

    Incorporated ProcessTTS Function

--- tts.go
+++ tts.go
@@ -32,12 +32,28 @@ type voice struct {
 
 func CheckErr(w http.ResponseWriter, e error, err_resp int) {
 	if e != nil {
-		println(e)
+		println(e.Error())
 		w.WriteHeader(err_resp) // tells our microservice what error response code to return
 	}
 }
 
-func TextToSpeech(w http.ResponseWriter, text []byte) []byte {
+func ExtractText(w http.ResponseWriter, r *http.Request) string {
+	t := map[string]interface{}{}
+	err := json.NewDecoder(r.Body).Decode(&t)
+	CheckErr(w, err, http.StatusBadRequest) // could not decode json response due to perceived client error
+
+	answer_text, ok := t["text"].(string)
+
+	if !ok {
+		err = errors.New("Object contains no field 'text'") // handle error for incorrect json object
+		CheckErr(w, err, http.StatusBadRequest)
+	}
+
+	println(answer_text)
+	return answer_text
+}
+
+func TextToSpeech(w http.ResponseWriter, text []byte) string {
 	client := &http.Client{}
 	req, err := http.NewRequest("POST", URI, bytes.NewBuffer(text))
 	CheckErr(w, err, http.StatusBadRequest) // the request was malformed
@@ -51,52 +67,17 @@ func TextToSpeech(w http.ResponseWriter, text []byte) []byte {
 
 	defer rsp.Body.Close() // defer ensures the response body is closed even in case of runtime error during parsing of response
 
-	// the request was successful
-	if rsp.StatusCode == http.StatusOK {
-		body, err := ioutil.ReadAll(rsp.Body)
-		CheckErr(w, err, http.StatusBadRequest) // the server cannot process the request due to something that is perceived to be a client error
-		return body
-	} else {
-		CheckStatusError(rsp.StatusCode) // long text error message
+	// the request was not successful
+	if rsp.StatusCode != http.StatusOK {
+		CheckStatusErr(rsp.StatusCode) // long text error message
 		err = errors.New("Cannot convert text to speech!")
 		CheckErr(w, err, rsp.StatusCode) // pass the microsoft error code to our own microservice response header
 	}
-	return nil
-}
 
-func CheckStatusError(err_status int) {
-	// https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/rest-text-to-speech
-	// error handling for each status code
-	// println(rsp.StatusCode)
-	if err_status == http.StatusBadRequest { // 400
-		println("Bad request - A required parameter is missing, empty, or null. " +
-			"Or, the value passed to either a required or optional parameter is invalid. " +
-			"A common reason is a header that's too long.")
-	}
-	if err_status == http.StatusUnauthorized { // 401
-		println("Unauthorized - The request is not authorized. " +
-			"Make sure your subscription key or token is valid and in the correct region.")
-	}
-	if err_status == http.StatusTooManyRequests { // 429
-		println("Too many requests - You have exceeded the quota or rate of requests allowed for your subscription.")
-	}
-	if err_status == http.StatusBadGateway { // 502
-		println("Bad gateway - There's a network or server-side problem. This status might also indicate invalid headers.")
-	}
-}
-
-func ExtractText(w http.ResponseWriter, r *http.Request) {
-	t := map[string]interface{}{}
-	if err := json.NewDecoder(r.Body).Decode(&t); err == nil {
-		CheckErr(w, err, http.StatusBadRequest) // the user sent and invalid request as the json format is incorrect
-		if answer_text, ok := t["text"].(string); ok {
-			println(answer_text)
-			ssml := CreateSSML(w, answer_text) // Speech Synthesis Markup Language
-			body := TextToSpeech(w, ssml)
-			encoded_speech := base64.StdEncoding.EncodeToString(body) // converts the string to base64 encoded wav
-			TTSResponse(w, encoded_speech)
-		}
-	}
+	answer_speech, err := ioutil.ReadAll(rsp.Body)
+	CheckErr(w, err, http.StatusBadRequest)                            // the server cannot process the request due to something that is perceived to be a client error
+	encoded_speech := base64.StdEncoding.EncodeToString(answer_speech) // converts the string to base64 encoded wav
+	return encoded_speech
 }
 
 func CreateSSML(w http.ResponseWriter, text string) []byte {
@@ -117,17 +98,45 @@ func CreateSSML(w http.ResponseWriter, text string) []byte {
 	return text_xml
 }
 
-func TTSResponse(w http.ResponseWriter, encoded_speech string) {
-	u := map[string]interface{}{"speech": encoded_speech}
+func CheckStatusErr(err_status int) {
+	// https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/rest-text-to-speech
+	// error handling for each status code
+	// println(rsp.StatusCode)
+	if err_status == http.StatusBadRequest { // 400
+		println("Bad request - A required parameter is missing, empty, or null. " +
+			"Or, the value passed to either a required or optional parameter is invalid. " +
+			"A common reason is a header that's too long.")
+	}
+	if err_status == http.StatusUnauthorized { // 401
+		println("Unauthorized - The request is not authorized. " +
+			"Make sure your subscription key or token is valid and in the correct region.")
+	}
+	if err_status == http.StatusTooManyRequests { // 429
+		println("Too many requests - You have exceeded the quota or rate of requests allowed for your subscription.")
+	}
+	if err_status == http.StatusBadGateway { // 502
+		println("Bad gateway - There's a network or server-side problem. This status might also indicate invalid headers.")
+	}
+}
+
+func TTSResponse(w http.ResponseWriter, answer_speech string) {
+	u := map[string]interface{}{"speech": answer_speech}
 	w.Header().Set("Content-Type", "application/json") // return microservice response as json
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(u)
 }
 
+func ProcessTTS(w http.ResponseWriter, r *http.Request) {
+	answer_text := ExtractText(w, r)
+	ssml := CreateSSML(w, answer_text) // Speech Synthesis Markup Language
+	answer_speech := TextToSpeech(w, ssml)
+	TTSResponse(w, answer_speech)
+}
+
 func TTSHandler() {
 	r := mux.NewRouter()
 	// document
-	r.HandleFunc("/tts", ExtractText).Methods("POST")
+	r.HandleFunc("/tts", ProcessTTS).Methods("POST")
 	http.ListenAndServe(":3003", r)
 	//	3001 / alpha
 	//	3002 / stt
