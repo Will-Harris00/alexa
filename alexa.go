36ec3add6c42e8eb2f6e2456253c25fc0d8157e5
Will-Harris00 <wjharris00@outlook.com>
2022-03-05 17:56:02 +0000

    Fixed response code error handling for alexa microservice

--- alexa.go
+++ alexa.go
@@ -8,103 +8,140 @@ import (
 	"net/http"
 )
 
-func CheckErr(w http.ResponseWriter, e error, errResp int) {
-	if e != nil {
-		println(e.Error())
-		// println(errResp)
-		w.WriteHeader(errResp) // tells our microservice what error response code to return
+func ProcessAlexa(w http.ResponseWriter, r *http.Request) {
+	sttRespBody, err, errCode := SpeechToTextManager(r)
+	if err != nil {
+		ErrorResponse(w, err, errCode) // return an error response from the microservice
+	}
+
+	alphaRespBody, err, errCode := AlphaManager(sttRespBody)
+	if err != nil {
+		ErrorResponse(w, err, errCode) // return an error response from the microservice
+	}
+
+	ttsRespBody, err, errCode := TextToSpeechManager(alphaRespBody)
+	if err != nil {
+		ErrorResponse(w, err, errCode) // return an error response from the microservice
+	} else {
+		AlexaResponse(w, ttsRespBody) // success
 	}
 }
 
-func ProcessAlexa(w http.ResponseWriter, r *http.Request) {
-	sttRespBody := SpeechToTextManager(w, r)
-	// return
-	alphaRespBody := AlphaManager(w, sttRespBody)
-	ttsRespBody := TextToSpeechManager(w, alphaRespBody)
-	AlexaResponse(w, ttsRespBody)
-}
-
-func SpeechToTextManager(w http.ResponseWriter, r *http.Request) []byte {
+func SpeechToTextManager(r *http.Request) ([]byte, error, int) {
 	sttUri := "http://localhost:3002/stt"
 
 	sttReq, err := http.NewRequest("POST", sttUri, r.Body)
-	CheckErr(w, err, http.StatusBadRequest) // the request was malformed
+	if err != nil {
+		return nil, err, http.StatusBadRequest // the request was malformed
+	}
 
 	sttReq.Header.Set("Content-Type", "application/json")
 
-	sttResp, err := http.DefaultClient.Do(sttReq)    // handle error for failed stt microservice query
-	CheckErr(w, err, http.StatusInternalServerError) // stt microservice could not be reached
-	// return nil
+	sttResp, err := http.DefaultClient.Do(sttReq) // handle error for failed stt microservice query
+	if err != nil {
+		return nil, err, http.StatusNotFound // stt microservice could not be reached
+	}
 
 	if sttResp.StatusCode != http.StatusOK {
-		err := errors.New("Something went wrong with the speech-to-text microservice!") // handle error for failed stt query
-		CheckErr(w, err, sttResp.StatusCode)                                            // pass the stt error code to the alexa microservice response header
+		err := errors.New("The speech-to-text microservice failed to handle the query!") // handle error for failed stt query
+		if err != nil {
+			return nil, err, sttResp.StatusCode // pass the stt error code to the alexa microservice response header
+		}
 	}
 
 	defer sttResp.Body.Close()
 
 	sttRespBody, err := ioutil.ReadAll(sttResp.Body) // read the body of the response returned from the stt microservice
-	CheckErr(w, err, http.StatusBadRequest)
+	if err != nil {
+		return nil, err, http.StatusInternalServerError // could not read the body of the response, perceived to be client error
+	}
+
 	println(string(sttRespBody)) // check response is received from speech-to-text microservice
 
-	return sttRespBody
+	return sttRespBody, nil, 0
 }
 
-func AlphaManager(w http.ResponseWriter, sttRespBody []byte) []byte {
+func AlphaManager(sttRespBody []byte) ([]byte, error, int) {
 	alphaUri := "http://localhost:3001/alpha"
 
 	alphaReq, err := http.NewRequest("POST", alphaUri, bytes.NewReader(sttRespBody))
-	CheckErr(w, err, http.StatusBadRequest) // the request was malformed
+	if err != nil {
+		return nil, err, http.StatusBadRequest // the request was malformed
+	}
 
 	alphaReq.Header.Set("Content-Type", "application/json")
 
 	alphaResp, err := http.DefaultClient.Do(alphaReq) // handle error for failed alpha query
-	CheckErr(w, err, http.StatusInternalServerError)  // alpha microservice could not be reached
+	if err != nil {
+		return nil, err, http.StatusNotFound // alpha microservice could not be reached
+	}
 
 	if alphaResp.StatusCode != http.StatusOK {
-		err := errors.New("Something went wrong with the alpha microservice!") // handle error for failed stt query
-		CheckErr(w, err, alphaResp.StatusCode)                                 // pass the stt error code to the alexa microservice response header
+		err := errors.New("The alpha microservice failed to handle the query!") // handle error for failed alpha query
+		if err != nil {
+			return nil, err, alphaResp.StatusCode // pass the alpha error code to the alexa microservice response header
+		}
 	}
 
 	defer alphaResp.Body.Close()
 
-	alphaRespBody, err := ioutil.ReadAll(alphaResp.Body) // read the body of the response returned from the stt microservice
-	CheckErr(w, err, http.StatusBadRequest)
-	println(string(alphaRespBody)) // check response is received from speech-to-text microservice
+	alphaRespBody, err := ioutil.ReadAll(alphaResp.Body) // read the body of the response returned from the alpha microservice
+	if err != nil {
+		return nil, err, http.StatusInternalServerError // could not read the body of the response, perceived to be client error
+	}
 
-	return alphaRespBody
+	println(string(alphaRespBody)) // check response is received from alpha microservice
+
+	return alphaRespBody, nil, 0
 }
 
-func TextToSpeechManager(w http.ResponseWriter, alphaRespBody []byte) []byte {
+func TextToSpeechManager(alphaRespBody []byte) ([]byte, error, int) {
 	ttsUri := "http://localhost:3003/tts"
 
 	ttsReq, err := http.NewRequest("POST", ttsUri, bytes.NewReader(alphaRespBody))
-	CheckErr(w, err, http.StatusBadRequest) // the request was malformed
+	if err != nil {
+		return nil, err, http.StatusBadRequest // the request was malformed
+	}
 
 	ttsReq.Header.Set("Content-Type", "application/json")
 
-	ttsResp, err := http.DefaultClient.Do(ttsReq)    // handle error for failed tts microservice query
-	CheckErr(w, err, http.StatusInternalServerError) // tts microservice could not be reached
+	ttsResp, err := http.DefaultClient.Do(ttsReq) // handle error for failed tts microservice query
+	if err != nil {
+		return nil, err, http.StatusNotFound // tts microservice could not be reached
+	}
 
 	if ttsResp.StatusCode != http.StatusOK {
-		err := errors.New("Something went wrong with the text-to-speech microservice!") // handle error for failed stt query
-		CheckErr(w, err, ttsResp.StatusCode)                                            // pass the stt error code to the alexa microservice response header
+		err := errors.New("The text-to-speech microservice failed to handle the request!") // handle error for failed tts query
+		if err != nil {
+			return nil, err, ttsResp.StatusCode // pass the tts error code to the alexa microservice response header
+		}
 	}
 
 	defer ttsResp.Body.Close()
 
-	ttsRespBody, err := ioutil.ReadAll(ttsResp.Body)
-	CheckErr(w, err, http.StatusBadRequest) // the server cannot process the request due to something that is perceived to be a client error
-	println(string(ttsRespBody))            // check response is received from speech-to-text microservice
+	ttsRespBody, err := ioutil.ReadAll(ttsResp.Body) // read the body of the response returned from the tts microservice
+	if err != nil {
+		return nil, err, http.StatusInternalServerError // could not read the body of the response, perceived to be client error
+	}
 
-	return ttsRespBody
+	println(string(ttsRespBody)) // check response is received from text-to-speech microservice
+
+	return ttsRespBody, nil, 0
 }
 
 func AlexaResponse(w http.ResponseWriter, ttsRespBody []byte) {
-	w.Header().Set("Content-Type", "application/json") // return microservice response as json
 	w.WriteHeader(http.StatusOK)
 	w.Write(ttsRespBody)
+	w.Header().Set("Content-Type", "application/json") // return microservice response as json
 	println("Play the answer.wav file to hear the solution to your question!")
+}
+
+func ErrorResponse(w http.ResponseWriter, err error, errCode int) {
+	w.WriteHeader(errCode)
+	w.Write([]byte(err.Error()))
+	w.Header().Set("Content-Type", "text") // return error message as text
+	println(errCode)
+	println(err.Error()) // display the error message on the console
 }
 
 func AlexaHandler() {
